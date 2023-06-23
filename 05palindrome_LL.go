4a28ee8c4f6a5c853d8ba91dafc07cc5d3b1b481
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2023-06-24 01:01:13 +0530

    Palindrome Linked List

---
+++ 05palindrome_LL.go
@@ -0,0 +1,41 @@
+// ! https://leetcode.com/problems/palindrome-linked-list/description/
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+var stack []ListNode
+
+func Push(str ListNode) {
+	stack = append(stack, str)
+}
+func Pop() {
+	n := len(stack) - 1
+	stack = stack[:n]
+}
+func isPalindrome(head *ListNode) bool {
+	one := head
+	two := head
+	for two != nil && two.Next != nil {
+		Push(*one)
+		two = two.Next.Next
+		if two != nil && two.Next == nil {
+			one = one.Next.Next
+		} else {
+			one = one.Next
+		}
+
+	}
+	for one != nil {
+		if one.Val != stack[len(stack)-1].Val {
+			return false
+		}
+		one = one.Next
+		Pop()
+	}
+	return true
+}
