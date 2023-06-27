2ebce924edf11f96aaf440ad9c3d00e2a4ecdd61
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2023-06-27 12:03:30 +0530

    Get pre and post order traversal

---
+++ 06n-ary_tree.go
@@ -0,0 +1,47 @@
+package main
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+ 
+func postorder(root *Node) []int {
+	ans := []int{}
+	if root == nil {
+		return ans
+	}
+	ans = getPostOrderTraversalForNaryTree(root, ans)
+	return ans
+}
+func getPostOrderTraversalForNaryTree(node *Node, ans []int) []int {
+	if len(node.Children) != 0 {
+		for i := 0; i < len(node.Children); i++ {
+			ans = getPostOrderTraversalForNaryTree(node.Children[i], ans)
+		}
+	}
+	if node != nil {
+		ans = append(ans, node.Val)
+	}
+	return ans
+}
+
+
+func preorder(root *Node) []int {
+	ans := []int{}
+	if root == nil {
+		return ans
+	}
+	ans = getPreOrderTraversalForNaryTree(root, ans)
+	return ans
+}
+func getPreOrderTraversalForNaryTree(node *Node, ans []int) []int {
+	if node != nil {
+		ans = append(ans, node.Val)
+	}
+	if len(node.Children) != 0 {
+		for i := 0; i < len(node.Children); i++ {
+			ans = getPreOrderTraversalForNaryTree(node.Children[i], ans)
+		}
+	}
+	return ans
+}
