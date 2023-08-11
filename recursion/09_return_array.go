faaf8a7a7066d5e5a436b770855735de2937e228
ArjunMalhotra07 <arjun03.malhotra@gmail.com>
2023-08-12 00:40:27 +0530

    Return all indices for occurence of an element

---
+++ recursion/09_return_array.go
@@ -0,0 +1,16 @@
+package recursion
+
+import "fmt"
+
+func CheckForRepitition(nums, ans []int, index, target int) []int {
+	if index == len(nums) {
+		return ans
+	}
+	if nums[index] == target {
+		ans = append(ans, index)
+	}
+	return CheckForRepitition(nums, ans, index+1, target)
+}
+func CheckForRepititionHelper() {
+	fmt.Println(CheckForRepitition([]int{1, 2, 3, 10, 10, 5}, []int{}, 0, 10))
+}
